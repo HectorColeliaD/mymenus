a099705c4bbb71e35ec05e12f84628e3cf11aa41
hector <coleliah64@gmail.com>
2025-09-01 11:31:20 -0300

    pequeño avance, pre-re hacer el componente de lista

--- burgers-menu/src/app/specialburgers-menu/sburgers/sburgers.ts
+++ burgers-menu/src/app/specialburgers-menu/sburgers/sburgers.ts
@@ -11,6 +11,6 @@ export class Sburgers {
   specialBurgers: Burgers[] =  [
     {id:1, burger: "Bacon doble", price: 4200},
     {id:2, burger: "Triple", price: 4200},
-    {id:3, burger: "Lechuga y Tomate", price: 4200},
+    {id:3, burger: "Completa", price: 4200},
   ]
 }
